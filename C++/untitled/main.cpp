ff16420942401efccd87d669df13ada56d407693
henry.rausch@st.ovgu.de <MzOM0bs!yyB2Tf5w$DHGxMB7C9#66A>
2021-01-29 19:04:17 +0100

    neue Dateien
    Erster Test mit Juniper Notebook

---
+++ C++/untitled/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // define veriables
+    float deposit, interest_rate, result;
+
+
+    cout << "Gewuenschter Endbetrag: ";
+    cin >> result;
+
+    cout << "Hoehe der Einmaleinlage: ";
+    cin >> deposit;
+
+    cout << "Zinssatz: ";
+    cin >> interest_rate;
+
+    //define starting point of the counter
+    int counter = 0;
+
+
+    while (deposit < result) {
+
+        //calculate percentage
+        deposit = deposit + (deposit * interest_rate / 100);
+
+        //count how many times the loop has been executed
+        counter++;
+    }
+
+    //print out the result
+    cout << "Bis zum gewuenschten Betrag dauert es " << counter << " Jahre \n";
+
+    getchar();
+    return 0;
+
+}
